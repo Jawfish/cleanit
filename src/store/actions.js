1efda87bc0ae8f6171566e5c6ec0f465e0789266
James Fitzgerald <jameswf@live.com>
2020-01-21 15:20:04 -0500

    removed empty file

--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-export const getPosts = (query, { commit }) => {
-  fetch(query)
-    .then(response => response.json())
-    .then(data =>
-      data.data.children.forEach(child => this.posts.push(child.data))
-    )
-}
